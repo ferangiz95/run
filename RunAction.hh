a814a68651c4e4b454bf7769bc6519492675967f
ferangiz95 <147204910+ferangiz95@users.noreply.github.com>
2023-11-27 23:16:51 +0300

    Add files via upload

---
+++ RunAction.hh
@@ -0,0 +1,20 @@
+#ifndef RunAction_HH
+#define RunAction_HH
+
+#include "G4UserRunAction.hh"
+#include "globals.hh"
+
+//class G4Run;
+
+class RunAction : public G4UserRunAction
+{
+  public:
+    RunAction();
+    ~RunAction() override = default;
+
+    void BeginOfRunAction(const G4Run*) override;
+    void EndOfRunAction(const G4Run*) override;
+};
+
+#endif
+
